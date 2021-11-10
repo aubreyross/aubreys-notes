d58810bee33c5c94c18d76ba920f21fa06cefa29
Aubrey Ross <aubrey.ross04@gmail.com>
2021-11-10 03:09:06 -0600

    new files

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-// Requires the following npm modules & dependencies 
-const express = require ('express');
-//uuid npm package for unique id 
-const {v4: uuidv4 } = require ('uuid');
-//import node path to indicate paths of routes
-const path = require ('path');
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const api = require('./routes/index.js');
 
-const fs = require('fs');
-
-// Express app configuration
-const app = express();
-//port to listen on for requests
 const PORT = process.env.PORT || 3001;
 
-// Express middleware to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
+
+// Middleware for parsing JSON and urlencoded form data 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
 app.use(express.static('public'));
 
-// Importing routes
-const apiRouter = require('./routes/apiroute.js');
-const htmlRouter = require('./routes/htmlroute.js');
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-app.use ('/api', apiRouter);
-app.use ('/', htmlRouter);
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+// Wildcard Route for homepage
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-// GET Routes to indicate where we're going to provide files and their location on our server's filesystem
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-//Home page
-app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
-
-//Note page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-
-//Wildcard 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-
-//Server accepts which port to listen on
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+// listener 
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
